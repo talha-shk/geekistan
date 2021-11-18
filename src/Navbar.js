30728b63e9b8a25cdde4c3fe7ce0395726ba4049
talha-shk <89351634+talha-shk@users.noreply.github.com>
2021-11-18 19:49:58 +0500

    third commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,30 @@ import './Navbar.css'
 import logo from './logo.png'
 
 function Navbar() {
-    return (
-      <>
+  const menu= () => {
+          console.log("Clicked")
+
+          var x = document.getElementById("mob-nav");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+  }
+  return (
+    <>
       <div className = 'nav-container'>
         <div className="nav-logo-container">
+            <div className="logo">
             <img src={logo} alt="Cant Load Image" />
             <h3>Infinite L<span className = 'red'>oo</span>pers</h3>
+            </div>
+            <div className="hamburger">
+            <i onClick = {menu} id = 'ham' class="fas fa-bars"/>
+            </div>
         </div>
-        <div className="nav-links-container">
-            <ul>
+        <div id = 'mob-nav' className="nav-links-container contain-nav">
+            <ul className = "show" id = 'mob-nav'>
                 <li><a href="./">Home</a></li>
                 <li><a href="./">Blog</a></li>
                 <li><a href="./">Contact</a></li>
